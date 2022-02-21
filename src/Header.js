98b355e3de47fafdf06dc331cfecc507be8122b6
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-21 15:43:31 -0800

    Added basic header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="text-center">
+        <h1>City Explorer</h1>
+        <hr />
+      </header>
+    );
+  }
+}
+
+export default Header;
