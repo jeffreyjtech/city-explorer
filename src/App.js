ed63ee3ad91ed13144b6a058e2e21c8dea3eadd1
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-21 16:08:39 -0800

    Added Main component to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
