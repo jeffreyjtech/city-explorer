c7f64aed1b9a8049e2c2321f54b7693dce738005
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-21 16:08:49 -0800

    Created Main component

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form"
+
+import Map from "./Map"
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <Map />
+        <Form>
+          
+        </Form>
+      </main>
+    );
+  }
+}
+
+export default Main;
