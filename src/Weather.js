ddcb64d4488bf68c6772ca1ac7071d492e80cdb7
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-23 00:59:10 -0800

    Added error logic and rendering based of Map.js

--- src/Weather.js
+++ src/Weather.js
@@ -8,15 +8,37 @@ class Weather extends React.Component {
       forecast = this.props.forecast :
       forecast = {datetime:'not found', description: 'not found'};
 
+    let error = this.props.forecastError;
+
+    let errorDisplay = (<div/>);
+    if(error) {
+      console.log('Trying to display error:',error)
+      if(error.response) {
+        if (error.response.data.message === 'Weather data not found') {
+          errorDisplay = (
+            <p className="bg-danger text-warning rounded p-1 mt-2">
+              Encountered error: <code>{error.response.data.message}</code>.
+            </p>
+          );
+        }
+      } else {       
+        errorDisplay = (
+          <p className="bg-danger text-warning rounded p-1 mt-2">
+            Encountered error: <code>{error.message}</code>. Try again.
+          </p>
+        );
+      } 
+    }
+
     return (
       <>
         <ListGroup className="mb-3">
-          <ListGroup.Item>Forecast time: {forecast.datetime}</ListGroup.Item>
+          <ListGroup.Item>Forecast time: {forecast.date}</ListGroup.Item>
           <ListGroup.Item>
             Forecast: {forecast.description}
           </ListGroup.Item>
         </ListGroup>
-        {/* {errorDisplay} */}
+        {errorDisplay}
       </>
     );
   };
