d979d1287660d304d922cb63e6fb65b3c2d32cbe
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-23 11:58:46 -0800

    Finished error display component (for now)

--- src/Error.js
+++ src/Error.js
@@ -4,32 +4,28 @@ import ListGroup from "react-bootstrap/ListGroup";
 class Error extends React.Component {
   render() {
     let errors = this.props.errors;
+    console.log(errors)
 
     let errorListItems = [];
-
-    for (let errorSource of Object.keys(errors)){
-      errorListItems.push(
-        <ListGroup.Item className="bg-danger text-warning rounded p-1 mt-2">
-          <code>{errorSource}</code>.
-        </ListGroup.Item>
-      );
-    }
     
-    for (let error of Object.values(errors)){
+    for (let errorObj of errors){
+      let error = errorObj.error;
+      let errorSource = errorObj.errorSource;
+
       console.log('Iterating on error:',error)
       if (error) {
         console.log('Trying to display error:', error.message);
-        if (error.response.data.error) {
+        if (error.message) {
           errorListItems.push(
             <ListGroup.Item className="bg-danger text-warning rounded p-1 mt-2">
-              Encountered error: <code>{error.response.status}: {error.response.data.error}</code>.
+              Encountered error from <b>{errorSource}</b> <code>{error.message}</code>.
             </ListGroup.Item>
           );
         
         } else if (typeof(error.response.data) === 'string'){
           errorListItems.push(
             <ListGroup.Item className="bg-danger text-warning rounded p-1 mt-2">
-              Encountered error: <code>{error.response.data}</code>. Try again.
+              Encountered error from <b>{errorSource}</b> <code>{error.response.data}</code>. Try again.
             </ListGroup.Item>
           );
         }
