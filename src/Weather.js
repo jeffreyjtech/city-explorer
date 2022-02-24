3a1ce5d1b0541d40bdb9f2ffceae5ebb9a61dd9a
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-23 20:18:44 -0800

    Forecast display elems now map from Main.js Forecasts

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,39 @@
 import React from "react";
+
 import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class Weather extends React.Component {
   render() {
-    let forecast;
-    this.props.forecast ?
-      forecast = this.props.forecast :
-      forecast = {datetime:'not found', description: 'not found'};
+    let forecast = this.props.forecast;
+    console.log(forecast);
 
-    return (
-      <>
+    let forecastElems = forecast.map(day => ( 
+      <Col>
         <ListGroup className="mb-3">
-          <ListGroup.Item>Forecast date: {forecast.date}</ListGroup.Item>
+          <ListGroup.Item>Forecast date: {day.date}</ListGroup.Item>
           <ListGroup.Item>
-            Forecast: {forecast.description}
+            Forecast: {day.description}
           </ListGroup.Item>
         </ListGroup>
-      </>
+      </Col>
+    ));
+
+    console.log(forecast)
+
+    return (
+      <Row>
+        {/* <Col>
+          <ListGroup className="mb-3">
+            <ListGroup.Item>Forecast date: {forecast.date}</ListGroup.Item>
+            <ListGroup.Item>
+              Forecast: {forecast.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col> */}
+        {forecastElems}
+      </Row>
     );
   };
 };
