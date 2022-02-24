9c8dcdbf72d716d8c95a2d5f8036d979c24a59a1
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-23 22:14:48 -0800

    Finished dynamic movie display carousel

--- src/Movie.js
+++ src/Movie.js
@@ -4,23 +4,44 @@ import Carousel from "react-bootstrap/Carousel";
 
 class Movie extends React.Component {
   render() {
+
+    /* movie is an array of Movie objects
+      Movie objects have the following shape
+    {
+      title: String (The movies' name)
+      overview: String (looooooong paragraph description)
+      vote_average: Int (not sure, idc)
+      vote_count: Int (number in the 1000s)
+      image_url: String (url formatted as string)
+      popularity: Int (number from 2 to 20 idk)
+      release_date: String (YYYY-MM-DD stored as string, w/ leading zeroes)
+    }
+    */
+    let movieDataArray = this.props.movies;
+
     let carouselELems = [];
 
-    carouselELems[0] = (
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/500x500"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>TITLE</h3>
-          <p>
-            PLACEHOLDER DINGUS
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    )
+    console.log(movieDataArray)
+    
+    if (movieDataArray){
+      carouselELems = movieDataArray.map(movie => (
+        <Carousel.Item key={movie.title}>
+          <img
+            className="d-block w-100"
+            src={movie.image_url}
+            alt={`Poster for movie ${movie.title}`}
+          />
+          <Carousel.Caption
+            className="m-auto shadow-lg rounded overflow-auto bg-info w-75 h-auto"
+          >
+            <h3>{movie.title}</h3>
+            <p>
+              {movie.overview}
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ));
+    }
 
     return(
       <Carousel 
