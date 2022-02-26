f0bf3197f1942a9f24175fb76ddb87f3915946de
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-25 16:24:53 -0800

    Formatted and added more robust children key-gen

--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React from 'react';
 
 // import ListGroup from "react-bootstrap/ListGroup";
-import Row from "react-bootstrap/Row";
+import Row from 'react-bootstrap/Row';
 // import Col from "react-bootstrap/Col";
-import WeatherDay from "./WeatherDay";
+import WeatherDay from './WeatherDay';
 
 class Weather extends React.Component {
   render() {
     let forecasts = this.props.forecasts;
+    let forecastElems = [];
 
-    let forecastElems = forecasts.map(day => ( 
-      <WeatherDay key={day.description} day={day} />
-    ));
+    if (forecasts[0].date) {
+      forecastElems = forecasts.map((day) => (
+        <WeatherDay key={day.description + day} day={day} />
+      ));
+    }
 
-    return (
-      <Row>
-        {forecastElems}
-      </Row>
-    );
-  };
-};
+    return <Row>{forecastElems}</Row>;
+  }
+}
 
 export default Weather;
